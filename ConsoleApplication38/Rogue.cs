b0367d2688687d887703347bd03dfdfe4ddc7651
Luke Grubbs <grubbs.luke@gmail.com>
2019-06-05 15:42:02 -0400

    Rouge Skills

--- ConsoleApplication38/Rogue.cs
+++ ConsoleApplication38/Rogue.cs
@@ -20,10 +20,37 @@ namespace ConsoleApplication38
             level = 0;
         }
 
-        public int VitalStrike()
+        
+        public void VitalStrike()
         {
-            //placeholder text
+            //Deals damage and applies bleed. (possible extra crit chance, get group consensus)
         }
+
+        public void Backstab()
+        {
+            //Uses strength for damage. 15% of speed is added to total damage dealt. Deals extra damage if target is dazed
+        }
+
+        public void Sprint()
+        {
+            //Increases speed stat by 30% for the next 3 or 4 turns
+        }
+
+        public int Snipe()
+        {
+            //Long ranged bow attack that deals extra damage to Imps.
+        }
+
+        public void TippedDagger()
+        {
+            //player throws a poison-tipped dagger that deals low damage, poisons 100%, and dazes 10%.
+        }
+        
+        public void Stealth()
+        {
+            //Defense is increased by 20% of current MP for 4 to 5 turns
+        }
+
     }
 }
             
