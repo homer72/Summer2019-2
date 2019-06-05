75458c7e44134f3cd3743b36d507a090b46d2490
Luke Grubbs <grubbs.luke@gmail.com>
2019-06-05 14:27:04 -0400

    Rogue

---
+++ ConsoleApplication38/Rogue.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication38
+{
+    class Rogue : Blank
+    {
+        public Rogue()
+        {
+            string job_name = "Rouge";
+            health = 15;
+            strength = 5;
+            defense = 0;
+            speed = 5 + (level * 2);
+            mp = 0;
+        }
+    }
+}
+            
+
