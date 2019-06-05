4f8a034ec0a2cad7695ebb74427bf8280bfd03ae
Lad12 <Luisdc12@gmail.com>
2019-06-05 14:00:22 -0400

    Warrior 2 test

---
+++ ConsoleApplication38/warrior2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication38
+{
+
+
+    class Warrior2 : Blank
+    {
+        public Warrior2()
+        {
+            health = 0;
+            strength = 5 + (level * 2);
+            defense = 0;
+            speed = 0;
+            mp = 0;
+        }
+    }
+}
+            
+
