4585fdddc872622ce192db8db59c621d6e898d0c
Luke Grubbs <grubbs.luke@gmail.com>
2019-06-12 14:38:34 -0400

    Tons of Rogue methods added

--- ConsoleApplication38/Rogue.cs
+++ ConsoleApplication38/Rogue.cs
@@ -20,18 +20,115 @@ namespace ConsoleApplication38
             level = 0;
         }
 
+        public void attack()
+        {
+            int choice = -1;
+            bool end = false;  //variable that determines when turn is over
+            while (!end) //loop that starts the turn
+            {
+                bool choose_phase = false;  //variable that determines the end of the 'pick a skill' phase
+                RogueSkills();
+                Console.Write("7. (Skill info)\n");
+
+                Console.WriteLine("Choose a skill from above: ");
+                while (!choose_phase)  //loop that starts the phase 'pick a skill' phase
+                {
+                    string input = Console.ReadLine();
+                    try
+                    {
+                        if (Convert.ToInt32(input) >=1 && Convert.ToInt32(input) <= 7)
+                        {
+                            choice = Convert.ToInt32(input);
+                        }
+                        choose_phase = true;
+                    }
+                    catch
+                    {
+                        RogueSkills();
+                        Console.WriteLine("Please type a number corresponding to the appropriate skill: ");
+                    }
+                }
+
+                switch (choice)
+                {
+                    case 1:
+                        VitalStrike();
+                        end = true;
+                        break;
+                    case 2:
+                        Backstab();
+                        end = true;
+                        break;
+                    case 3:
+                        Sprint();
+                        end = true;
+                        break;
+                    case 4:
+                        Snipe();
+                        end = true;
+                        break;
+                    case 5:
+                        TippedDagger();
+                        end = true;
+                        break;
+                    case 6:
+                        Stealth();
+                        end = true;
+                        break;
+                    case 7:
+                        SkillInfo();
+                        break;
+                    default:
+                        Console.WriteLine("Something went wrong when picking a skill. You shouldn't be able to see this message.");
+                        break;
+                }
+            }
+        }
+
+        public void updateStats(int damage, int HP, int Str, int Def, int Spe, int MP)
+        {
+            health = HP - damage;
+            strength = Str;
+            defense = Def;
+            speed = Spe;
+            mp = MP;
+        }
+
         public void ViewStats()
         {
             Console.WriteLine("Health: {0}\nStrength: {1}\nDefense: {2}\nSpeed: {3}\nMP: {4}\n", health, strength, defense, speed, mp);
         }
         
-        public void RougeSkills()
+        public void RogueSkills()
         {
-            Console.WriteLine("1. Vital Strike\n2. Backstab\n3. Sprint\n4. Snipe\n5. Tipped Dagger\n6.Stealth");
+            Console.WriteLine("1. Vital Strike\n2. Backstab\n3. Sprint\n4. Snipe\n5. Tipped Dagger\n6. Stealth\n");
         }
 
-        public void SkillInfo(int skill)
+        public void SkillInfo()
         {
+            int skill = -1;
+            RogueSkills();
+            Console.Write("7. (Quit)\n");
+            Console.WriteLine("Pick a skill you would like to know more about:");
+            while (true)
+            {
+                string input = Console.ReadLine();
+                try
+                {
+                    if (Convert.ToInt32(input) >= 1 && Convert.ToInt32(input) <= 7)
+                    {
+                        skill = Convert.ToInt32(input);
+                    }
+                    break;
+                }
+                catch
+                {
+                    RogueSkills();
+                    Console.Write("7. (Quit)\n");
+                    Console.WriteLine("Please type a number corresponding to the appropriate skill: ");
+                }
+            }
+
             switch (skill)
             {
                 case 1:
@@ -51,6 +148,9 @@ namespace ConsoleApplication38
                     break;
                 case 6:
                     Console.WriteLine("Defense is increased by 40% of current MP for 4 to 5 turns");
+                    break;
+                case 7:
+                    Console.WriteLine("Choose a skill: ");
                     break;
                 default:
                     Console.WriteLine("That is not one of the Rouge's skills.");
